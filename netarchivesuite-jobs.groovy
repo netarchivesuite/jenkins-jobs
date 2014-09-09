62979a8c499e2f9d0638dbeaa9b998760b9cf41e
Mikis Seth Sorensen <mss@statsbiblioteket.dk>
2014-09-09 10:27:01 +0200

    First attempt at a dsl

--- netarchivesuite-jobs.groovy
+++ netarchivesuite-jobs.groovy
@@ -1,33 +1,39 @@
 def project = 'NetarchiveSuite'
+def giturl = 'https://github.com/netarchivesuite/netarchivesuite.git'
+def branchBuilds = ['master', 'NAS-2383']
 
-def giturl = 'https://github.com/netarchivesuite/netarchivesuite/'
+def javaVersion = 'java8'
+def mavenVersion = 'maven3.2'
 
-def branchBuilds = ['master']
+branchBuilds.each {
+    def branch = it
+    job(type: Maven) {
+        name "NetarchiveSuite-${branch}"
+        description "Branch ${branch} build"
+        scm {
+            git(giturl, branch)
+        }
+        jdk javaVersion
+        mavenInstallation mavenVersion
+        goals "clean install -PfullTest"
+        authorization {
+            permission('hudson.model.Run.Update:netarchivesuite-devel')
+            permission('hudson.model.Item.Build:netarchivesuite-devel')
+            permission('hudson.model.Item.Read:anonymous')
+            permission('hudson.model.Item.Read:netarchivesuite-devel')
+            permission('hudson.model.Item.Workspace:netarchivesuite-devel')
+            permission('hudson.model.Item.Release:netarchivesuite-devel')
+            permission('hudson.model.Item.Discover:netarchivesuite-devel')
+            permission('hudson.model.Run.Delete:netarchivesuite-devel')
+            permission('hudson.model.Item.Cancel:netarchivesuite-devel')
+            permission('hudson.model.Item.Configure:netarchivesuite-devel')
+        }
+        triggers {
+            githubPush()
+        }
 
-
-def it = 'master'
-//branchBuilds.each {
-job {
-    name '${project}-${it}'
-    description 'Default build on the branch ${it} branch'
-    scm {
-        git {
-            remote {
-                url($ { giturl }, $ { it })
-            }
+        publishers {
+            mailer('', true, true)
         }
     }
-    steps {
-        maven("clean install -PfullTest")
-    }
-//    triggers {
-//        githubPush()
-//    }
-//    jdk('java8')
-
-//    mavenInstallation('maven3.2')
-//    publishers {
-//        mailer('', false, true)
-//    }
-//    }
 }
