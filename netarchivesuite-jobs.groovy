e284a5b7eb9bb8967aea03429ca18684aa4f870a
Mikis Seth Sorensen <mss@statsbiblioteket.dk>
2014-09-10 12:39:09 +0200

    Disabling artifact archiving for branch projects

--- netarchivesuite-jobs.groovy
+++ netarchivesuite-jobs.groovy
@@ -14,21 +14,33 @@ branchBuilds.each {
 
         goals "clean test -PfullTest"
 
-        publishers {
-            archiveArtifacts ''
+        configure { job ->
+            def publishers = job / publishers
+            publishers.setValue('')
         }
     }
 }
 
 job(type: Maven) {
     using nas
-    name "${nas}-sonar-dsl"
-    description "Full build with publishing off code analysis to SBForge Sonar"
+    name "${nas}-sonar"
+    description "Full build with publishing of code analysis to SBForge Sonar"
 
     triggers {
         cron('0 0 * * *')
     }
-    goals = 'sonar:sonar'
+    configure { project ->
+        project / publishers << 'hudson.plugins.sonar.SonarPublisher' {
+            jdk('(Inherit From Job)')
+            branch()
+            language()
+            mavenOpts()
+            jobAdditionalProperties()
+            settings(class: 'jenkins.mvn.DefaultSettingsProvider')
+            globalSettings(class: 'jenkins.mvn.DefaultGlobalSettingsProvider')
+            usePrivateRepository(false)
+        }
+    }
 }
 
 job(type: Maven) {
