088d4c3cf08d5bba8e154585ee9fe209ceefc25b
Mikis Seth Sorensen <mss@statsbiblioteket.dk>
2014-09-15 10:12:13 +0200

    Postponing deploy of NAS artifacts to Systemtest has passed

--- netarchivesuite-jobs.groovy
+++ netarchivesuite-jobs.groovy
@@ -1,37 +1,35 @@
 def nas = 'NetarchiveSuite'
+def template = nas+'-template'
 def giturl = 'https://github.com/netarchivesuite/netarchivesuite.git'
-def branchBuilds = ['NAS-2383', 'NAS-2124', 'H1']
+def branchBuilds = ['master', 'NAS-2383', 'NAS-2124', 'H1']
 
 branchBuilds.each {
     def branch = it
     job(type: Maven) {
-        using nas
+        using template
         name "${nas}-${branch}"
         description "Branch ${branch} build"
         scm {
             git(giturl, branch)
         }
+        triggers {
+            githubPush()
+        }
 
         goals "clean test -PfullTest"
-
-        configure { job ->
-            (job / publishers).setValue('')
-        }
     }
 }
 
 job(type: Maven) {
-    using nas
+    using template
     name "${nas}-sonar"
     description "Full build with publishing of code analysis to SBForge Sonar"
 
-    configure { job ->
-        (job / triggers).setValue('')
-    }
+    goals "clean install -PfullTest"
+
     triggers {
         cron('0 0 * * *')
     }
-
 
     configure { project ->
         project / publishers << 'hudson.plugins.sonar.SonarPublisher' {
@@ -48,7 +46,7 @@ job(type: Maven) {
 }
 
 job(type: Maven) {
-    using nas
+    using template
     name "${nas}-system-test"
     description "Check out the latest version from github at midnight and:\n" +
             "<ul>\n" +
@@ -58,13 +56,24 @@ job(type: Maven) {
             "</ul>\n" +
             "  <b>Target: mvn clean install -PsystemTest -rf integration-test</b>  "
 
-    goals "clean install -PsystemTest -rf integration-test"
+    goals "clean integration-test -PsystemTest -rf integration-test"
 
-
-    configure { job ->
-        (job / triggers).setValue('')
-    }
     triggers {
         cron('0 0 * * *')
+    }
+
+    publishers {
+        downstream("${nas}-publish-artifacts", 'SUCCESS')
+    }
+}
+
+job(type: Maven) {
+    using template
+    name "${nas}-publish-artifacts"
+    description "Publishes NAS artifacts if the system test passes."
+
+    goals "clean deploy -PskipTests"
+    publishers {
+        archiveArtifacts 'deploy/releasezipball/target/NetarchiveSuite*.zip'
     }
 }
